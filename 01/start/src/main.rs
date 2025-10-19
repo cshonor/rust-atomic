fe6d5c966c12524de44999718bdc012bc7213361
wzp <1239263644@qq.com>
2025-10-19 19:29:05 +0800

    创建一个进程，用thread.spawn 创建，unwarp进行判断

--- 01/start/src/main.rs
+++ 01/start/src/main.rs
@@ -1,3 +1,14 @@
+use std::thread;
 fn main() {
     println!("Hello, world!");
+    let handle = thread::spawn(f);
+    handle.join().unwrap();
+}
+
+fn f()  {
+ 
+  println!("Hello from the thread!");
+    let id=thread::current().id();
+    println!("Thread id is: {:?}", id);
+
 }
