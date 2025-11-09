4fc16a912af4f05ce3b976e988abd20723deaa1d
wzp <1239263644@qq.com>
2025-11-09 17:27:59 +0800

    更新 move 示例并新增 usescope

--- ex1/start/src/move.rs
+++ ex1/start/src/move.rs
@@ -1,2 +1,13 @@
 use std::thread;
 //用 闭包
+fn main() {
+    let x = 1;
+    let y = 2;
+    let z = 3;
+    let f =thread::spawn(move || {
+        println!("x: {}", x);
+        println!("y: {}", y);
+        println!("z: {}", z);
+    };
+    f.join().unwrap();
+}
